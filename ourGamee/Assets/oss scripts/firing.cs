acb53658bfaa26e2d70eca5ecd443e58c60a611a
OssamaElshikh <ossamaelshikh66@gmail.com>
2023-12-14 04:04:33 +0200

    adding shotgun

--- ourGamee/Assets/oss scripts/firing.cs
+++ ourGamee/Assets/oss scripts/firing.cs
@@ -1,14 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class firing : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public GameObject shotgunbullet;
     public float bulletSpeed = 10;
-    public string weapon = "pistol";
+    public int weapon = 1;
     public StarterAssets.StarterAssetsInputs st;
     public Transform bulletPoint;
+    public Transform shotguntrans;
+
 
 
     void Update()
@@ -16,21 +21,32 @@ public class firing : MonoBehaviour
         //firing
         if (st.isAiming == false)
         {
+
+
+
+            if (Input.GetKeyDown(KeyCode.K) && weapon == 2)
             {
+                // Create a rotation Quaternion with a 90-degree rotation around the Y-axis
+                Quaternion bulletRotation = Quaternion.Euler(0f, 0f, 90f);
 
+                // Instantiate the bullet prefab with the specified rotation
+                var bullet = Instantiate(bulletPrefab, bulletPoint.position, bulletRotation);
 
-                if (Input.GetKeyDown(KeyCode.K) && weapon == "pistol")
+                // Set the velocity of the bullet
+                bullet.GetComponent<Rigidbody>().velocity = bulletPoint.forward * bulletSpeed;
+            }
+            if (Input.GetKeyDown(KeyCode.K) && weapon == 1)
                 {
                     // Create a rotation Quaternion with a 90-degree rotation around the Y-axis
                     Quaternion bulletRotation = Quaternion.Euler(0f, 0f, 90f);
 
                     // Instantiate the bullet prefab with the specified rotation
-                    var bullet = Instantiate(bulletPrefab, bulletPoint.position, bulletRotation);
+                    var bullet = Instantiate(shotgunbullet, shotguntrans.position, bulletRotation);
 
                     // Set the velocity of the bullet
-                    bullet.GetComponent<Rigidbody>().velocity = bulletPoint.forward * bulletSpeed;
+                    bullet.GetComponent<Rigidbody>().velocity = shotguntrans.forward * bulletSpeed;
                 }
-            }
+            
         }
     }
 }
